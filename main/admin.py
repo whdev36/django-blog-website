240ad1278a0dfcb40827156a8b246609c5a44610
Wisdom Hunter <whdev36@gmail.com>
2025-02-05 08:55:26 -0800

    tag and category models

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import Post
+from .models import Post, Category, Tag
 
 
 # Register
@@ -8,7 +8,18 @@ class PostAdmin(admin.ModelAdmin):
 	list_display = ['title', 'author', 'created_at', 'updated_at', 'is_published']
 	list_filter = ['is_published', 'created_at']
 	search_fields = ['title', 'content']
+	filter_horizontal = ('tags',)
+
+class TagAdmin(admin.ModelAdmin):
+	list_display = ['name']
+	search_fields = ['name']
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug']
+	search_fields = ['name', 'slug']
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 admin.site.unregister(Group)
--- main/models.py
+++ main/models.py
@@ -1,12 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import F
+from django.utils.text import slugify
+
+
+# --- CATEGORY MODEL ---
+class Category(models.Model):
+	class Meta:
+		verbose_name = 'Category'
+		verbose_name_plural = 'Categories'
+
+	name = models.CharField(max_length=150)
+	slug = models.SlugField(max_length=150, blank=True)
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = slugify(self.name.lower())
+		super().save(*args, **kwargs)
+
+	def __str__(self):
+		return self.name
+
+class Tag(models.Model):
+	class Meta:
+		verbose_name = 'Tag'
+		verbose_name_plural = 'Tags'
+
+	name = models.CharField(max_length=150)
+
+	def __str__(self):
+		return self.name
 
 # --- POST MODEL ---
 class Post(models.Model):
 	title = models.CharField(max_length=255)  # title
 	content = models.TextField()  # content
 	author = models.ForeignKey(User, on_delete=models.CASCADE)  # author
+	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)  # category
+	tags = models.ManyToManyField(Tag, related_name='posts', blank=True)  # tags
 	created_at = models.DateTimeField(auto_now_add=True)  # created date
 	updated_at = models.DateTimeField(auto_now=True)  # updated date
 	is_published = models.BooleanField(default=True)  # is publish
@@ -14,8 +45,8 @@ class Post(models.Model):
 	views = models.PositiveIntegerField(default=0)  # view 
 
 	class Meta:
-		# verbose_name = 'Post'
-		# verbose_name_plural = 'Posts'
+		verbose_name = 'Post'
+		verbose_name_plural = 'Posts'
 		ordering = ['-created_at']
 
 	def __str__(self):
