833ca42ab056f6ca3a5d29d31d1d3bb3150bb40b
Wisdom Hunter <whdev36@gmail.com>
2025-02-05 21:27:08 -0800

    update

--- main/models.py
+++ main/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import F
 from django.utils.text import slugify
+import markdown
+import bleach
 
 
 # --- CATEGORY MODEL ---
@@ -9,13 +11,14 @@ class Category(models.Model):
 	class Meta:
 		verbose_name = 'Category'
 		verbose_name_plural = 'Categories'
+		ordering = ['name']
 
-	name = models.CharField(max_length=150)
-	slug = models.SlugField(max_length=150, blank=True)
+	name = models.CharField(max_length=150, unique=True)  # name
+	slug = models.SlugField(max_length=150, blank=True, unique=True)  # category
 
 	def save(self, *args, **kwargs):
 		if not self.slug:
-			self.slug = slugify(self.name.lower())
+			self.slug = slugify(self.name)
 		super().save(*args, **kwargs)
 
 	def __str__(self):
@@ -25,8 +28,9 @@ class Tag(models.Model):
 	class Meta:
 		verbose_name = 'Tag'
 		verbose_name_plural = 'Tags'
+		ordering = ['name']
 
-	name = models.CharField(max_length=150)
+	name = models.CharField(max_length=150)  # name
 
 	def __str__(self):
 		return self.name
@@ -35,9 +39,10 @@ class Tag(models.Model):
 class Post(models.Model):
 	title = models.CharField(max_length=255)  # title
 	content = models.TextField()  # content
-	author = models.ForeignKey(User, on_delete=models.CASCADE)  # author
-	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)  # category
+	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')  # author
+	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')  # category
 	tags = models.ManyToManyField(Tag, related_name='posts', blank=True)  # tags
+	sources = models.TextField(blank=True, null=True)  # sources
 	created_at = models.DateTimeField(auto_now_add=True)  # created date
 	updated_at = models.DateTimeField(auto_now=True)  # updated date
 	is_published = models.BooleanField(default=True)  # is publish
@@ -53,15 +58,37 @@ class Post(models.Model):
 		return self.title
 
 	def add_clap(self, commit=True):
-		self.claps += 1
-		self.save()
+		self.claps = F('claps') + 1
+		self.save(update_fields=['claps'])
 
 	def add_view(self, commit=True):
+		# self.views = F('views') + 1
+		# self.save(update_fields=['views'])
 		self.views += 1
 		self.save()
 
 	@classmethod
-	def get_trending_posts(cls):
+	def get_trending_posts(cls, limit=10):
 		return cls.objects.annotate(
 			total_interactions=F('views') + F('claps')
-			).order_by('-total_interactions', '-created_at')
+		).order_by('-total_interactions', '-created_at')[:limit]
+
+	def get_markdown_content(self):
+		raw_html = markdown.markdown(self.content, extensions=['extra'])
+		allowed_tags = ['p', 'a', 'b', 'i', 'strong', 'em', 'ul', 'ol', 'li', 'code',
+			'img', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'br']
+		allowed_attributes = {
+			'a': ['href', 'title'],
+			'img': ['src', 'alt', 'width', 'height']
+		}
+		return bleach.clean(raw_html, tags=allowed_tags, attributes=allowed_attributes)
+
+	def get_markdown_sources(self):
+		if not self.sources:
+			return ""
+		raw_html = markdown.markdown(self.sources, extensions=['extra'])
+		allowed_tags = ['p', 'a', 'b', 'i', 'strong', 'em', 'ul', 'ol', 'li', 'code']
+		allowed_attributes = {
+			'a': ['href', 'title'],
+		}
+		return bleach.clean(raw_html, tags=allowed_tags, attributes=allowed_attributes)
--- main/views.py
+++ main/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .models import Post
 from django.http import JsonResponse
+from django.db.models import F
 
 # --- HOME PAGE ---
 def home(request):
 	# posts = Post.objects.filter(is_published=True)
-	posts = Post.get_trending_posts()[:10]
+	posts = Post.get_trending_posts()
 	return render(request, 'home.html', {'posts': posts})
 
 # --- POSTS ---
@@ -25,3 +26,7 @@ def clap(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	post.add_clap()
 	return redirect('post', pk=post.pk)
+	# post.claps = F('claps') + 1
+	# post.save(update_fields=['claps'])
+	# post.refresh_from_db()
+	# return JsonResponse({'success': True, 'claps': post.claps})
